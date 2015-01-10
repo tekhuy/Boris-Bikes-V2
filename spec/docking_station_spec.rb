934cac3ec7ba5398f00ce215b8387e800a78406d
tekhuy <huyle20@hotmail.com>
2015-01-10 19:12:40 +0000

    created docking_station class

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,44 @@
+require 'docking_station'
+require 'bike'
+
+describe DockingStation do
+  
+  let(:station){DockingStation.new(capacity: 20)}
+  let(:bike){Bike.new}
+
+  def fill_station(station)
+    20.times { station.dock(bike) }
+  end
+
+  it 'should accept a bike' do
+    expect(station.bike_count).to eq(0)
+    station.dock(bike)
+    expect(station.bike_count).to eq(1)
+  end
+
+  it 'should release a bike' do
+    station.dock(bike)
+    station.release(bike)
+    expect(station.bike_count).to eq(0)
+  end
+
+  it 'should know when it\'s full' do
+    expect(station).not_to be_full
+    fill_station station
+    expect(station).to be_full
+  end
+
+  it 'should not accept a bike if it\'s full' do
+    fill_station station
+    expect(lambda { station.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
+  end
+
+  it 'should provide the list of available bikes' do
+    working_bike, broken_bike = Bike.new, Bike.new
+    broken_bike.break!
+    station.dock(working_bike)
+    station.dock(broken_bike)
+    expect(station.available_bikes).to eq([working_bike])
+  end
+
+end
